25c2a7813593c27cf536fdf9f5312877bfd5de4a
Bipin Budhathoki <bipinbudhathoki2014@gmail.com>
2024-02-07 23:24:56 +0545

    update script for deploy

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,9 +3,11 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
+echo "goto working dir"
+cd /var/www/html
 echo "npm install and build"
 npm install
-npm run build
+npm run build 
 
 echo "generating application key..."
 php artisan key:generate --show
