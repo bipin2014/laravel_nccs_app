1f6385c2df91c947fd40939fd0c80eed3d90e492
Bipin Budhathoki <bipinbudhathoki2014@gmail.com>
2024-02-09 16:15:51 +0545

    remove route comments

--- routes/web.php
+++ routes/web.php
@@ -23,55 +23,55 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-// Route::resource('chirps', ChirpController::class)
-//     ->only(['index', 'store', 'edit', 'update'])
-//     ->middleware(['auth', 'verified']);
-
-
-
-// Route::middleware('log')->group(function () {
-
-//     Route::controller(HelloController::class)->group(function () {
-//         Route::get("/hello", "index");
-//         Route::get(
-//             "/user/{id}/comments/{comments}",
-//             "showUser"
-//         )->whereNumber("comments");
-
-//         Route::get('/hello/hi', "helloHi");
-//     });
+Route::resource('chirps', ChirpController::class)
+    ->only(['index', 'store', 'edit', 'update'])
+    ->middleware(['auth', 'verified']);
 
 
-//     Route::view("/welcome", "welcome");
-//     Route::redirect("/hi", "/hello");
-// });
 
-// Route::prefix("admin", function () {
-//     Route::get("/hello", [HelloController::class, "index"]);
-//     Route::get('/hello/hi', [HelloController::class, "helloHi"]);
-// });
+Route::middleware('log')->group(function () {
 
-// Route::prefix("user")->group(function () {
-//     Route::get("/hello", [HelloController::class, "index"]);
-//     Route::get('/hello/hi', [HelloController::class, "helloHi"]);
-// });
+    Route::controller(HelloController::class)->group(function () {
+        Route::get("/hello", "index");
+        Route::get(
+            "/user/{id}/comments/{comments}",
+            "showUser"
+        )->whereNumber("comments");
 
-// Route::resource('laptop', LaptopController::class)->middleware('auth');
-// Route::resource('mobile', MobileController::class)->middleware('auth');
+        Route::get('/hello/hi', "helloHi");
+    });
 
-// Route::get('mobiles', [MobileController::class, 'layoutDemo']);
-// Route::get('mobiles/new', [MobileController::class, 'layoutDemoNew'])
-//     ->name('mobile.new');
+
+    Route::view("/welcome", "welcome");
+    Route::redirect("/hi", "/hello");
+});
+
+Route::prefix("admin", function () {
+    Route::get("/hello", [HelloController::class, "index"]);
+    Route::get('/hello/hi', [HelloController::class, "helloHi"]);
+});
+
+Route::prefix("user")->group(function () {
+    Route::get("/hello", [HelloController::class, "index"]);
+    Route::get('/hello/hi', [HelloController::class, "helloHi"]);
+});
+
+Route::resource('laptop', LaptopController::class)->middleware('auth');
+Route::resource('mobile', MobileController::class)->middleware('auth');
+
+Route::get('mobiles', [MobileController::class, 'layoutDemo']);
+Route::get('mobiles/new', [MobileController::class, 'layoutDemoNew'])
+    ->name('mobile.new');
 
 
 Route::get('nccs/about', [NccsController::class,'about']);
